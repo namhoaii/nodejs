179e7a3fe6536444f5d21fd5caece8b05b8e44a4
Hoai Nam <hoainamsmurf@gmail.com>
2023-07-14 08:03:18 +0700

    commit model

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 let app = express();
@@ -14,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connectDB();
 
 app.listen(port, () => {
     console.log(`Backend Nodejs is running on the port: ${port}`);
